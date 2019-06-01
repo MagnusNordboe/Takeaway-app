a910c744f493912ca5a897fee37e3a7b89e82884
Magnus Nordbø <nordbom@live.no>
2019-06-01 15:22:46 +0200

    no message

---
+++ www/precache-manifest.bcb81b49a79dabaa44ecae54e84bdc9d.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88219dd9796cca0bfa90e472a805ab9",
+    "url": "static/icons/favicon_old.png"
+  },
+  {
+    "revision": "a1e60c1dbe93340c05b0f420df9ccef3",
+    "url": "static/Logologo_hard.png"
+  },
+  {
+    "revision": "9b69f56a2947d59e7bd2901bd69c792e",
+    "url": "static/dmoimg/Viet-thai1.png"
+  },
+  {
+    "revision": "9fa40ac188452505f216044f8c7a9b88",
+    "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "0dfa6c62175cf391c50b0e4ac3d0d815",
+    "url": "static/dmoimg/Galleri1.png"
+  },
+  {
+    "revision": "f403549ed2ede2e76c619ea4e0b4408d",
+    "url": "static/dmoimg/burgerhub1.png"
+  },
+  {
+    "revision": "1add30d23be9344f4ece25af0dfd3683",
+    "url": "static/dmoimg/Happtime1.png"
+  },
+  {
+    "revision": "b5654a6ea217bb31fe77bf26cf4ee167",
+    "url": "static/dmoimg/dampen1.png"
+  },
+  {
+    "revision": "a8ab4981714858c31c2e276d2126ea6d",
+    "url": "static/dmoimg/Ikkosushi1.png"
+  },
+  {
+    "revision": "a21d42fd1ff5b7426ab12e739a88c8a8",
+    "url": "static/dmoimg/Kebabish1.png"
+  },
+  {
+    "revision": "7914da433e87f847137812df94aeb0ef",
+    "url": "static/dmoimg/happytimecafe1.png"
+  },
+  {
+    "revision": "203ed85614922823f9fbd20cdcb51447",
+    "url": "static/local_firstlogo.png"
+  },
+  {
+    "revision": "593e65b66dd13ceecc10d008fe0b3820",
+    "url": "static/icons/favicon.png"
+  },
+  {
+    "revision": "09bac65cbc73f494482c480006f7c31a",
+    "url": "static/icons/152x152.png"
+  },
+  {
+    "revision": "78bb1bad56b56f9176976fbb19f640d8",
+    "url": "static/icons/144x144.png"
+  },
+  {
+    "revision": "b08f600e75284d6274ee0ee03f2ed1b3",
+    "url": "static/icons/128x128.png"
+  },
+  {
+    "revision": "14235e3018bb57cc700c799c5f9ef1be",
+    "url": "static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "1c41296078623fe115c3b966852a9759",
+    "url": "static/icons/192x192.png"
+  },
+  {
+    "revision": "ad809d0a19d880fc189e41e9726df58e",
+    "url": "static/icons/256x256.png"
+  },
+  {
+    "revision": "111371010ef70bbcf7e6968abc8e2fb2",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "6b32ee47d87641889a9ba09ae2585c07",
+    "url": "static/LocalLogo.png"
+  },
+  {
+    "revision": "28db829912bb402df5b1e6fb16f1908c",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "72deb27df87082f6b74076bf44f2594d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "fb4837ce6da2f7ac7b2e",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "19158ff09eb907432a7ccacd074ef9d5",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "5fce7c1238bad823bee064bd127fe4cd",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "fb4837ce6da2f7ac7b2e",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "e9edb9aecde00929125157d646a61bff",
+    "url": "./index.html"
+  }
+]);
